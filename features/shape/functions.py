272c09d9c9dbfd31728ed58aa012d6b458faf9a7
bianconif <bianco@ieee.org>
2021-08-04 17:54:11 +0200

    features/shape/functions/shape_index function updated

--- features/shape/functions.py
+++ features/shape/functions.py
@@ -17,6 +17,13 @@ def shape_index(a, b, c, index):
         dimensions of the ROI as well as the axes of the inertia ellipsoid.
     index : str
         A string indicating the index requested. Possible values are:
+            - 'cailleux-flatness' (Cailleux flatness)
+            - 'csi' (Corey shape index)
+            - 'disc-rod' (Disc-rod index)
+            - 'krumbein-spericity' (Krumbein sphericity)
+            - 'mps' (Maximum projection sphericity)
+            - 'oblate-prolate' (Oblate-prolate index)
+            - 'wentworth-flatness' (Wentworth flatness)
             
         
     Returns
@@ -25,10 +32,20 @@ def shape_index(a, b, c, index):
         The value of the shape index.
     """
     value = None
-    if index == 'disc-rod':
+    if index == 'cailleux-flatness':
+        value = 1000 * (a + b) / (2 * c)
+    elif index == 'csi':
+        value = c / ((a * b) ** (1/2))
+    elif index == 'disc-rod':
         value = (a - b)/(a - c)
+    elif index == 'krumbein-sphericity':
+        value = ((c * b) / (a ** 2)) ** (1/3)   
+    elif index == 'mps':
+        value = (c**2 / (a*b)) ** (1/3)
     elif index == 'oblate-prolate':
         value = 10 * ((a - b)/(a - c) - 1/2) / (c/a) 
+    elif index == 'wentworth-flatness':
+        value = (a + b)/c
     else:
         raise Exception('Shape index not supported')
 
